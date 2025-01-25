2af72c37832ed4b13ce4510d47431e5f14f12044
gjasay <gabeasay@gmail.com>
2025-01-25 14:01:20 -0500

    fetch username

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
 import { Hyperlink } from "../components/Hyperlink";
+import supabase from "../utils/supabase";
+import { User } from "@supabase/supabase-js";
 
 export const Header: React.FC = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const user = await supabase.auth.getUser();
+      if (user.error) {
+        console.error(user.error.message);
+        return;
+      } else {
+        setUser(user.data.user);
+      }
+    })();
+  }, []);
+
   return (
     <header className="flex justify-between items-center w-full py-4 border-b-zinc-900 border-b-4">
       <h1 className="px-8 font-extrabold size-xl">RVA Games</h1>
@@ -10,8 +27,13 @@ export const Header: React.FC = () => {
         <Hyperlink href="/forum" text="Forums" />
       </div>
       <div className="flex justify-evenly items-center h-full w-1/8">
-        <Hyperlink href="/login" text="Login" />
-        <Hyperlink href="/register" text="Register" />
+        {user && <Hyperlink href="/user-settings" text={user.user_metadata.username} />}
+        {!user && (
+          <>
+            <Hyperlink href="/login" text="Login" />
+            <Hyperlink href="/register" text="Register" />
+          </>
+        )}
       </div>
     </header>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,3 @@
-import { useEffect, useState } from "react"
-import supabase from "../utils/supabase"
-
 export const Home: React.FC = () => {
-  const [names, setNames] = useState<string[]>([])
-
-  useEffect(() => {
-    async function getNames() {
-      const { data: test } = await supabase.from('test').select();
-
-      if (test && test.length > 0) {
-        const newNames = test.map((name) => name.name);
-        setNames(newNames);
-      }
-    }
-    getNames();
-  }, []);
-
-  return (
-    <div>
-      {names.map((todo) => (
-        <li key={todo}>{todo}</li>
-      ))}
-    </div>
-  )
-}
+  return <div></div>;
+};
