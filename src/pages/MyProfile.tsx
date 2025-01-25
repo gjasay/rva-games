c4049eca686921489c2a8bc836015c8ea1d09e16
Isaac Gifford <geekingreen@gmail.com>
2025-01-25 15:48:00 -0700

    moar stuff

--- src/pages/MyProfile.tsx
+++ src/pages/MyProfile.tsx
@@ -1,29 +1,10 @@
-import { useEffect, useState } from "react";
 import supabase from "../utils/supabase";
-import { User } from "@supabase/supabase-js";
 import { Button } from "../components/Button";
 import { useNavigate } from "react-router-dom";
-
-const useSupabaseUser = () => {
-  const navigate = useNavigate();
-  const [user, setUser] = useState<User | null>(null);
-
-  useEffect(() => {
-    supabase.auth.getUser().then((res) => {
-      if (res.error) {
-        navigate("/login");
-      } else {
-        setUser(res.data.user);
-      }
-    });
-  }, []);
-
-  return user;
-};
+import useSupabaseUser from "../hooks/useSupabaseUser";
 
 export const MyProfile: React.FC = () => {
   const navigate = useNavigate();
-
   const user = useSupabaseUser();
 
   const handleLogout = async () => {
@@ -33,14 +14,14 @@ export const MyProfile: React.FC = () => {
 
   return (
     <div className="flex justify-center items-center h-full">
-      {user && (
+      {user.value && (
         <div className="flex flex-col justify-around items-center bg-zinc-900 border-violet-500 border-4 rounded-2xl">
           <h1 className="text-xl font-extrabold py-5 border-b-4 border-violet-500 text-center align-middle">
             My Profile
           </h1>
           <div className="flex flex-col items-center justify-center h-6/8">
-            <p className="text-xl">Username: {user.user_metadata.username}</p>
-            <p className="text-xl">Email: {user.email}</p>
+            <p className="text-xl">Username: {user.value.user_metadata.username}</p>
+            <p className="text-xl">Email: {user.value.email}</p>
             <Button onClick={() => navigate("/upload-game")}>
               Upload Game
             </Button>
