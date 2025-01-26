2a4be78e8a4a4ee45cad7fc07003da3ac754cd3c
gjasay <gabeasay@gmail.com>
2025-01-26 14:48:13 -0500

    upload profile picture and set user avatar_url

--- src/pages/MyProfile.tsx
+++ src/pages/MyProfile.tsx
@@ -2,33 +2,88 @@ import supabase from "../utils/supabase";
 import { Button } from "../components/Button";
 import { useNavigate } from "react-router-dom";
 import useSupabaseUser from "../hooks/useSupabaseUser";
-import { Hyperlink } from "../components/Hyperlink";
 import { Card } from "../components/Card";
+import { useState } from "react";
 
 export const MyProfile: React.FC = () => {
+  const [updatePicture, setUpdatePicture] = useState(false);
+  const [picture, setPicture] = useState<File | undefined>(undefined);
+  const [success, setSuccess] = useState("");
+  const [error, setError] = useState("");
+
   const navigate = useNavigate();
-  const user = useSupabaseUser();
+  const user = useSupabaseUser().value;
 
   const handleLogout = async () => {
     await supabase.auth.signOut();
     navigate("/");
   };
 
+  const handlePhotoSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!picture || !user) return;
+
+    const { data: url, error } = await supabase.storage
+      .from("avatars")
+      .upload(`${user.id}/${picture.name}`, picture);
+    if (error) {
+      console.error(error);
+      setError(error.message);
+      return;
+    } else {
+      setSuccess("Uploading...");
+      console.log(url);
+    }
+
+    supabase.auth
+      .updateUser({
+        data: {
+          avatar_url: `https://aopplcfhmbnprixvhihr.supabase.co/storage/v1/object/public/${url.fullPath}`,
+          username: user.user_metadata.username,
+        },
+      })
+      .then((res) => {
+        if (res.error) {
+          setError(res.error.message);
+        } else {
+          setSuccess("Successfully updated picture.");
+        }
+      });
+
+    setUpdatePicture(false);
+  };
+
   return (
     <div className="flex justify-center items-center h-full">
-      {user.value && (
+      {user && (
         <Card title="My Profile">
           <div className="flex flex-col items-center justify-center gap-4 p-4">
             <img
-              src={user.value.user_metadata.avatar_url}
+              src={user.user_metadata.avatar_url}
               alt="avatar"
               className="rounded-full h-24 w-24"
-              />
-            <Hyperlink href="/edit-profile" text="Edit Profile" />
-            <p className="text-xl">
-              Username: {user.value.user_metadata.username}
-            </p>
-            <p className="text-xl">Email: {user.value.email}</p>
+            />
+            {!updatePicture && (
+              <Button onClick={() => setUpdatePicture(!updatePicture)}>
+                Change Picture
+              </Button>
+            )}
+            {error && <p className="text-red-500">{error}</p>}
+            {success && <p className="text-green-500">{success}</p>}
+            {updatePicture && (
+              <form
+                className="flex flex-col justfify-center items-center"
+                onSubmit={handlePhotoSubmit}
+              >
+                <input
+                  type="file"
+                  onChange={(e) => setPicture(e.target.files?.[0])}
+                />
+                <Button type="submit">Upload</Button>
+              </form>
+            )}
+            <p className="text-xl">Username: {user.user_metadata.username}</p>
+            <p className="text-xl">Email: {user.email}</p>
             <div className="flex justify-between gap-4">
               <Button onClick={() => navigate("/upload-game")}>
                 Upload Game
